c8e864b30470a0c3c9ab75847c1f184a098beada
Alexander Larsson <alexl@src.gnome.org>
2007-09-13 11:41:34 +0000

    Use a generic main for the daemons
    Original git commit by Alexander Larsson <alex@greebo.(none)> at 1171293699 +0100
    
    svn path=/trunk/; revision=324

---
+++ daemon/daemon-main-generic.c
@@ -0,0 +1,26 @@
+#include <config.h>
+
+#include <glib.h>
+#include "daemon-main.h"
+#include G_STRINGIFY(BACKEND_HEADER)
+
+int
+main (int argc, char *argv[])
+{
+  daemon_init ();
+  daemon_main (argc, argv,
+#ifdef DEFAULT_BACKEND_TYPE
+	       G_STRINGIFY(DEFAULT_BACKEND_TYPE),
+#else
+	       NULL,
+#endif
+#ifdef MOUNTABLE_DBUS_NAME
+	       G_STRINGIFY(MOUNTABLE_DBUS_NAME),
+#else
+	       NULL,
+#endif
+	       BACKEND_TYPES
+	       NULL);
+	       
+  return 0;
+}
