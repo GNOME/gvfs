7c0a239dc80249a3ffef195784eeb27f625547e9
Alexander Larsson <alexl@src.gnome.org>
2007-09-13 13:18:38 +0000

    Rename base_class property to base_stream
    Original git commit by Christian Kellner <gicmo@gnome.org> at 1178451894 +0200
    
    svn path=/trunk/; revision=509

--- gio/gfilteroutputstream.c
+++ gio/gfilteroutputstream.c
@@ -94,9 +94,9 @@ g_filter_output_stream_class_init (GFilterOutputStreamClass *klass)
 
   g_object_class_install_property (object_class,
                                    PROP_BASE_STREAM,
-                                   g_param_spec_object ("base_class",
-                                                         P_("The Filter Base Class"),
-                                                         P_("The underlying base class the io ops will be done on"),
+                                   g_param_spec_object ("base_stream",
+                                                         P_("The Filter Base Stream"),
+                                                         P_("The underlying base stream the io ops will be done on"),
                                                          G_TYPE_OUTPUT_STREAM,
                                                          G_PARAM_READWRITE | G_PARAM_CONSTRUCT_ONLY | 
                                                          G_PARAM_STATIC_NAME|G_PARAM_STATIC_NICK|G_PARAM_STATIC_BLURB));
