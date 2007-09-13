e03599c3adb612966e51b591ed9298c1bddbe593
Alexander Larsson <alexl@src.gnome.org>
2007-09-13 15:05:29 +0000

    Converted uri mapping modules to use GIOModule. This involves creating a GVfsUriMapper baseclass in common/
    Original git commit by Alexander Larsson <alexl@redhat.com> at 1188821530 +0200
    
    svn path=/trunk/; revision=884

---
+++ common/gvfsurimapper.c
@@ -0,0 +1,63 @@
+#include <config.h>
+#include <string.h>
+#include <gmodule.h>
+#include "gvfsurimapper.h"
+
+G_DEFINE_TYPE (GVfsUriMapper, g_vfs_uri_mapper, G_TYPE_OBJECT);
+
+static void
+g_vfs_uri_mapper_class_init (GVfsUriMapperClass *klass)
+{
+}
+
+static void
+g_vfs_uri_mapper_init (GVfsUriMapper *vfs)
+{
+}
+
+const char **
+g_vfs_uri_mapper_get_handled_schemes (GVfsUriMapper  *mapper)
+{
+  GVfsUriMapperClass *class;
+
+  class = G_VFS_URI_MAPPER_GET_CLASS (mapper);
+
+  return (* class->get_handled_schemes) (mapper);
+}
+
+  
+void
+g_vfs_uri_mapper_from_uri (GVfsUriMapper  *mapper,
+			   GDecodedUri    *uri,
+			   GMountSpec    **spec_out,
+			   char          **path_out)
+{
+  GVfsUriMapperClass *class;
+
+  class = G_VFS_URI_MAPPER_GET_CLASS (mapper);
+
+  return (* class->from_uri) (mapper, uri, spec_out, path_out);
+}
+
+const char **
+g_vfs_uri_mapper_get_handled_mount_types (GVfsUriMapper  *mapper)
+{
+  GVfsUriMapperClass *class;
+  
+  class = G_VFS_URI_MAPPER_GET_CLASS (mapper);
+
+  return (* class->get_handled_mount_types) (mapper);
+}
+
+void
+g_vfs_uri_mapper_to_uri (GVfsUriMapper  *mapper,
+			 GMountSpec     *spec,
+			 char           *path,
+			 GDecodedUri    *uri)
+{
+  GVfsUriMapperClass *class;
+  
+  class = G_VFS_URI_MAPPER_GET_CLASS (mapper);
+  
+  return (* class->to_uri) (mapper, spec, path, uri);
+}
---
+++ common/gvfsurimapper.h
@@ -0,0 +1,59 @@
+#ifndef __G_VFS_URI_MAPPER_H__
+#define __G_VFS_URI_MAPPER_H__
+
+#include <glib.h>
+#include <gvfsuriutils.h>
+#include <gmountspec.h>
+
+G_BEGIN_DECLS
+
+#define G_TYPE_VFS_URI_MAPPER         (g_vfs_uri_mapper_get_type ())
+#define G_VFS_URI_MAPPER(o)           (G_TYPE_CHECK_INSTANCE_CAST ((o), G_TYPE_VFS_URI_MAPPER, GVfsUriMapper))
+#define G_VFS_URI_MAPPER_CLASS(k)     (G_TYPE_CHECK_CLASS_CAST((k), G_TYPE_VFS_URI_MAPPER, GVfsUriMapperClass))
+#define G_VFS_URI_MAPPER_GET_CLASS(o) (G_TYPE_INSTANCE_GET_CLASS ((o), G_TYPE_VFS_URI_MAPPER, GVfsUriMapperClass))
+#define G_IS_VFS(o)        (G_TYPE_CHECK_INSTANCE_TYPE ((o), G_TYPE_VFS_URI_MAPPER))
+#define G_IS_VFS_CLASS(k)  (G_TYPE_CHECK_CLASS_TYPE ((k), G_TYPE_VFS_URI_MAPPER))
+
+typedef struct _GVfsUriMapper         GVfsUriMapper; /* Dummy typedef */
+typedef struct _GVfsUriMapperClass    GVfsUriMapperClass;
+
+struct _GVfsUriMapper {
+  GObject parent;
+};
+
+struct _GVfsUriMapperClass
+{
+  GObjectClass parent_class;
+
+  /* Virtual Table */
+
+  const char ** (*get_handled_schemes)     (GVfsUriMapper *mapper);
+  void          (*from_uri)                (GVfsUriMapper *mapper,
+					    GDecodedUri *uri,
+					    GMountSpec **spec_out,
+					    char **path_out);
+  
+  const char ** (*get_handled_mount_types) (GVfsUriMapper *mapper);
+  void          (*to_uri)                  (GVfsUriMapper *mapper,
+					    GMountSpec *spec,
+					    char *path,
+					    GDecodedUri *uri);
+};
+
+GType g_vfs_uri_mapper_get_type (void) G_GNUC_CONST;
+
+const char **g_vfs_uri_mapper_get_handled_schemes     (GVfsUriMapper  *mapper);
+void         g_vfs_uri_mapper_from_uri                (GVfsUriMapper  *mapper,
+						       GDecodedUri    *uri,
+						       GMountSpec    **spec_out,
+						       char          **path_out);
+
+const char **g_vfs_uri_mapper_get_handled_mount_types (GVfsUriMapper  *mapper);
+void         g_vfs_uri_mapper_to_uri                  (GVfsUriMapper  *mapper,
+						       GMountSpec     *spec,
+						       char           *path,
+						       GDecodedUri    *uri);
+
+G_END_DECLS
+
+#endif /* __G_VFS_URI_MAPPER_H__ */
