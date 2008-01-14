40b929f565ed9a974f093931700d0b3b26c8abc5
Alexander Larsson <alexl@redhat.com>
2008-01-14 14:56:09 +0000

    Handle root correctly in g_file_get_relative_path (#508719)
    2008-01-14  Alexander Larsson  <alexl@redhat.com>
    
            * client/gdaemonfile.c:
            (match_prefix):
    	Handle root correctly in g_file_get_relative_path (#508719)
    
    
    svn path=/trunk/; revision=1123

--- client/gdaemonfile.c
+++ client/gdaemonfile.c
@@ -332,6 +332,14 @@ match_prefix (const char *path, const char *prefix)
   prefix_len = strlen (prefix);
   if (strncmp (path, prefix, prefix_len) != 0)
     return NULL;
+
+  /* Handle the case where prefix is the root, so that
+   * the IS_DIR_SEPRARATOR check below works */
+  if (prefix_len > 0 &&
+      prefix[prefix_len-1] == '/')
+    prefix_len--;
+  
+  
   return path + prefix_len;
 }
 
