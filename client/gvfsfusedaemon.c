c2aa528c17d2d4ef9e236b41b0c26d641ea11d6b
Alexander Larsson <alexl@redhat.com>
2008-10-16 16:54:31 +0000

    Show username in auth dialog if specified in uri (#554156)
    2008-10-16  Alexander Larsson  <alexl@redhat.com>
    
            * daemon/gvfsbackendftp.c:
            (do_mount):
    	Show username in auth dialog if specified in uri (#554156)
    
    2008-10-16  Alexander Larsson  <alexl@redhat.com>
    
    	* client/gvfsfusedaemon.c (getattr_for_file):
    	Set st_blocks & co so that du works (#554682)
    	Patch from Andreas Henriksson
    
    
    
    svn path=/trunk/; revision=2052

--- client/gvfsfusedaemon.c
+++ client/gvfsfusedaemon.c
@@ -744,6 +744,13 @@ getattr_for_file (GFile *file, struct stat *sbuf)
       if (g_file_info_has_attribute (file_info, G_FILE_ATTRIBUTE_TIME_ACCESS))
         sbuf->st_atime = file_info_get_attribute_as_uint (file_info, G_FILE_ATTRIBUTE_TIME_ACCESS);
 
+      if (g_file_info_has_attribute (file_info, G_FILE_ATTRIBUTE_UNIX_BLOCK_SIZE))
+	sbuf->st_blksize = file_info_get_attribute_as_uint (file_info, G_FILE_ATTRIBUTE_UNIX_BLOCK_SIZE);
+      if (g_file_info_has_attribute (file_info, G_FILE_ATTRIBUTE_UNIX_BLOCKS))
+	sbuf->st_blocks = file_info_get_attribute_as_uint (file_info, G_FILE_ATTRIBUTE_UNIX_BLOCKS);
+      else /* fake it to make 'du' work like 'du --apparent'. */
+	sbuf->st_blocks = (sbuf->st_size + 1) / 512;
+
       /* Setting st_nlink to 1 for directories makes 'find' work */
       sbuf->st_nlink = 1;
 
