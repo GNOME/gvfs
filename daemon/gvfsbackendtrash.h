ca00fc23cc8ae0ddb302347da7196c9923afc9a1
Ryan Lortie <desrt@desrt.ca>
2008-12-15 19:24:01 +0000

    relicense to LGPLv2+
    2008-12-15  Ryan Lortie  <desrt@desrt.ca>
    
            * daemon/gvfsbackendtrash.c:
            * daemon/gvfsbackendtrash.h: relicense to LGPLv2+
    
    
    svn path=/trunk/; revision=2138

--- daemon/gvfsbackendtrash.h
+++ daemon/gvfsbackendtrash.h
@@ -2,8 +2,11 @@
  * Copyright © 2008 Ryan Lortie
  *
  * This program is free software: you can redistribute it and/or modify
- * it under the terms of version 3 of the GNU General Public License as
- * published by the Free Software Foundation.   
+ * it under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation; either version 2 of the licence or (at
+ * your option) any later version.
+ *
+ * See the included COPYING file for more information.
  */
 
 #ifndef _gvfsbackendtrash_h_
