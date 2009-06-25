2eb9e35ccf5112a6f01225c97b05e90eca8c7ea9
Alexander Larsson <alexl@redhat.com>
2009-06-25 19:39:06 +0200

    Add header include protection and boilerplate

--- metadata/metatree.h
+++ metadata/metatree.h
@@ -1,3 +1,28 @@
+/* GIO - GLib Input, Output and Streaming Library
+ *
+ * Copyright (C) 2009 Red Hat, Inc.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General
+ * Public License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place, Suite 330,
+ * Boston, MA 02111-1307, USA.
+ *
+ * Author: Alexander Larsson <alexl@redhat.com>
+ */
+
+#ifndef __META_TREE_H__
+#define __META_TREE_H__
+
 #include <glib.h>
 
 typedef struct _MetaTree MetaTree;
@@ -78,3 +103,4 @@ gboolean    meta_tree_remove           (MetaTree                         *tree,
 gboolean    meta_tree_copy             (MetaTree                         *tree,
 					const char                       *src,
 					const char                       *dest);
+#endif /* __META_TREE_H__ */
