66df9e764aed43b23b5c3326ff9ab024b0a49124
Andreas Henriksson <andreas@fatal.se>
2009-07-19 17:36:14 +0200

    Make debug output for progress callbacks work with large files
    Without this patch, the debug output would easily overflow, as it was
    cast to int.

--- daemon/gvfsjobmove.c
+++ daemon/gvfsjobmove.c
@@ -132,7 +132,7 @@ g_vfs_job_move_progress_callback (goffset current_num_bytes,
   dbus_uint64_t current_dbus, total_dbus;
   DBusMessage *message;
 
-  g_debug ("progress_callback %d/%d\n", (int)current_num_bytes, (int)total_num_bytes);
+  g_debug ("progress_callback " G_GOFFSET_FORMAT "/" G_GOFFSET_FORMAT "\n", current_num_bytes, total_num_bytes);
 
   if (op_job->callback_obj_path == NULL)
     return;
