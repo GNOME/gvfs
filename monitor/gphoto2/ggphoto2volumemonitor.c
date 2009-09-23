0285e9ede48ff83aead882401e36e9d7ae25746f
Alexander Larsson <alexl@redhat.com>
2009-09-23 13:07:46 +0200

    FreeBSD support for gphoto2 backend
    Here is a patch from Aurélien Jarno to enable the gphoto2 backend on FreeBSD
    and GNU/kFreeBSD.

--- monitor/gphoto2/ggphoto2volumemonitor.c
+++ monitor/gphoto2/ggphoto2volumemonitor.c
@@ -763,7 +763,13 @@ update_cameras (GGPhoto2VolumeMonitor *monitor,
         continue;
 
       usb_bus_num = hal_device_get_property_int (d, "usb.bus_number");
+#if defined(__linux__)
       usb_device_num = hal_device_get_property_int (d, "usb.linux.device_number");
+#elif defined(__FreeBSD__) || defined(__FreeBSD_kernel__)
+      usb_device_num = hal_device_get_property_int (d, "freebsd.unit");
+#else
+# error "Need OS specific tweaks"
+#endif
 
       store_heads = get_stores_for_camera (usb_bus_num, usb_device_num);
       num_store_heads = g_list_length (store_heads);
