1c75aff19b7fb06e1d49de3fb108c648da4428be
David Zeuthen <davidz@redhat.com>
2012-01-17 15:17:33 -0500

    Fix off-by-one when parsing mount options with a value of length 1
    Signed-off-by: David Zeuthen <davidz@redhat.com>

--- monitor/udisks2/gvfsudisks2utils.c
+++ monitor/udisks2/gvfsudisks2utils.c
@@ -99,7 +99,7 @@ gvfs_udisks2_utils_lookup_fstab_options_value (const gchar *fstab_options,
           start += strlen (key);
           for (n = 0; start[n] != ',' && start[n] != '\0'; n++)
             ;
-          if (n > 1)
+          if (n >= 1)
             ret = g_uri_unescape_segment (start, start + n, NULL);
         }
     }
