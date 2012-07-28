b0bdbda5baf3ecd710f25515e2f37dd531491cf2
Matthias Clasen <mclasen@redhat.com>
2012-07-28 07:46:27 +0200

    Fix a comment typo

--- common/gvfsdaemonprotocol.h
+++ common/gvfsdaemonprotocol.h
@@ -72,7 +72,7 @@ G_BEGIN_DECLS
 #define G_VFS_DBUS_OP_GET_CONNECTION "GetConnection"
 #define G_VFS_DBUS_OP_CANCEL "Cancel"
 
-/* Used by the dbus-proxying implementation of GMoutOperation */
+/* Used by the dbus-proxying implementation of GMountOperation */
 #define G_VFS_DBUS_MOUNT_OPERATION_INTERFACE "org.gtk.vfs.MountOperation"
 #define G_VFS_DBUS_MOUNT_OPERATION_OP_ASK_PASSWORD "askPassword"
 #define G_VFS_DBUS_MOUNT_OPERATION_OP_ASK_QUESTION "askQuestion"
