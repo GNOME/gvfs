6645adf71cca81ad59eef7bbcf47cfdab505fb9b
Philip Langdale <philipl@cloudera.com>
2012-08-02 10:06:57 -0700

    MTP: Make gphoto2 monitor skip MTP devices.

--- monitor/gphoto2/ggphoto2volumemonitor.c
+++ monitor/gphoto2/ggphoto2volumemonitor.c
@@ -201,6 +201,11 @@ gudev_add_camera (GGPhoto2VolumeMonitor *monitor, GUdevDevice *device, gboolean 
 	return;
       }
 #endif /* HAVE_AFC */
+    if (g_udev_device_get_property_as_boolean (device, "ID_MTP_DEVICE"))
+      {
+	/* g_debug ("ignoring device, is AFC"); */
+	return;
+      }
 
     usb_bus_num = g_udev_device_get_property (device, "BUSNUM");
     if (usb_bus_num == NULL) {
