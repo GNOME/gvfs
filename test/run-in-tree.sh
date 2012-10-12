909f82902f6ca89290eba6f454e0b5b56d0d1408
Martin Pitt <martinpitt@gnome.org>
2012-10-12 12:20:32 +0200

    test/run-in-tree.sh: Search session.conf in test directory
    Allow users to call test/run-in-tree.sh from any directory, so that you can e.
    g. run "test/run-in-tree.sh gvfs-mount -li". Without this, run-in-tree.sh only
    finds its session.conf when running this in the tests/ directory.

--- test/run-in-tree.sh
+++ test/run-in-tree.sh
@@ -12,9 +12,11 @@ export GVFS_MONITOR_DIR=`pwd`
 export PATH=`pwd`/../programs:$PATH
 export GIO_EXTRA_MODULES=`pwd`/../client/.libs:`pwd`/../monitor/proxy/.libs
 
+DBUS_CONF=`dirname $0`/session.conf
+
 # Start a custom session dbus
 PIDFILE=`mktemp`
-export DBUS_SESSION_BUS_ADDRESS=`dbus-daemon --config-file=$(pwd)/session.conf --fork --print-address=1 --print-pid=3 3>${PIDFILE}`
+export DBUS_SESSION_BUS_ADDRESS=`dbus-daemon --config-file=$DBUS_CONF --fork --print-address=1 --print-pid=3 3>${PIDFILE}`
 DBUS_SESSION_BUS_PID=`cat $PIDFILE`
 rm $PIDFILE
 
