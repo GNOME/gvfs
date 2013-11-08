GNOME/gvfs

commit 15d5da3f13f9325cab8d32316304ce49d0ff83fb

trash: Add " (invalid encoding)" to files with invalid encoding
https://bugzilla.gnome.org/show_bug.cgi?id=656186

daemon/gvfsbackendtrash.c
@@ -493,16 +493,15 @@ trash_backend_add_info (TrashItem *item,
 
       if (original)
         {
-          gchar *basename, *path;
+          gchar *edit_name, *path;
 
           path = g_file_get_path (original);
-          basename = g_filename_display_basename (path);
+          edit_name = gvfs_file_info_populate_names_as_local (info, path);
 
-          g_file_info_set_display_name (info, basename);
           g_file_info_set_attribute_byte_string (info,
                                                  G_FILE_ATTRIBUTE_TRASH_ORIG_PATH,
                                                  path);
-          g_free (basename);
+          g_free (edit_name);
           g_free (path);
         }
 

commit 9d90065bad034ab17fe10bb8bde9dd0979d7050a

trash: Fix missing include
Caused by 15d5da3f13f9325cab8d32316304ce49d0ff83fb.

daemon/gvfsbackendtrash.c
@@ -24,6 +24,7 @@
 #include "gvfsjobenumerate.h"
 #include "gvfsjobseekread.h"
 #include "gvfsjobread.h"
+#include "gvfsdaemonutils.h"
 
 typedef GVfsBackendClass GVfsBackendTrashClass;
 