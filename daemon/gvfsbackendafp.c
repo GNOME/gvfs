GNOME/gvfs

commit 91cf41328a2df886ec59c533ffa4991f92059e59

afp: Don't free the handle if truncate fails
https://bugzilla.gnome.org/show_bug.cgi?id=573837

daemon/gvfsbackendafp.c
@@ -795,7 +795,6 @@ truncate_cb (GObject *source_object, GAsyncResult *res, gpointer user_data)
   {
     g_vfs_job_failed_from_error (G_VFS_JOB (job), err);
     g_error_free (err);
-    afp_handle_free (afp_handle);
   }
 }
 

commit dd03740d26da5d730d91c2c9bec3257b009380a4

afp: Remove unused variable
Remove a set but not used variable caused by 91cf41328a2d (afp: Don't
free the handle if truncate fails).

daemon/gvfsbackendafp.c
@@ -783,7 +783,6 @@ truncate_cb (GObject *source_object, GAsyncResult *res, gpointer user_data)
 {
   GVfsAfpVolume *volume = G_VFS_AFP_VOLUME (source_object);
   GVfsJobTruncate *job = G_VFS_JOB_TRUNCATE (user_data);
-  AfpHandle *afp_handle = (AfpHandle *)job->handle;
 
   GError *err = NULL;
 