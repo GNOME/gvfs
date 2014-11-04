GNOME/gvfs

commit 2a0dc68c7eeb9a945860b68a295d5c1d9101bd0f

client: Remove inconsistencies between async and sync variants
The async implementation of find_enclosing_mount does not assume that
there is a GMountInfo when no error has occurred.

https://bugzilla.gnome.org/show_bug.cgi?id=739075

client/gdaemonfile.c
@@ -2278,8 +2278,18 @@ g_daemon_file_find_enclosing_mount (GFile *file,
 						  daemon_file->path,
 						  cancellable,
 						  error);
-  if (mount_info == NULL)
+
+  if (error)
     goto out;
+
+  if (mount_info == NULL)
+    {
+      g_set_error (error, G_IO_ERROR,
+                   G_IO_ERROR_FAILED,
+                   "Internal error: \"%s\"",
+                   "No error but no mount info from g_daemon_vfs_get_mount_info_sync");
+      goto out;
+    }
 
   if (mount_info->user_visible)
     {

commit 7757c2bcfef808c860ec0e8676d69301a0f821d1

client: Fix up error handling in find_enclosing_mount
2a0dc68c7eeb ("client: Remove inconsistencies between async and sync
variants") introduced a regression which causes find_enclosing_mount to
segfault if a non-NULL GError** is passed in.  Fix this.  Also, only
call g_dbus_error_strip_remote_error () on GErrors that have come from
dbus.

https://bugzilla.gnome.org/show_bug.cgi?id=739075

client/gdaemonfile.c
@@ -2279,8 +2279,11 @@ g_daemon_file_find_enclosing_mount (GFile *file,
 						  cancellable,
 						  error);
 
-  if (error)
-    goto out;
+  if (error && *error)
+    {
+      g_dbus_error_strip_remote_error (*error);
+      return NULL;
+    }
 
   if (mount_info == NULL)
     {
@@ -2288,7 +2291,7 @@ g_daemon_file_find_enclosing_mount (GFile *file,
                    G_IO_ERROR_FAILED,
                    "Internal error: \"%s\"",
                    "No error but no mount info from g_daemon_vfs_get_mount_info_sync");
-      goto out;
+      return NULL;
     }
 
   if (mount_info->user_visible)
@@ -2310,10 +2313,6 @@ g_daemon_file_find_enclosing_mount (GFile *file,
      corresponding to a particular path/uri */
 		       _("Could not find enclosing mount"));
  
-out:
-  if (error && *error)
-    g_dbus_error_strip_remote_error (*error);
-
   return NULL;
 }
 