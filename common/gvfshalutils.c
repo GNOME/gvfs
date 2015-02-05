32377d138d4d28f5da5ed0935d4b90fa2647c1ec
Ting-Wei Lan <lantw@src.gnome.org>
2015-02-05 11:57:08 +0800

    Fix build error when using HAL backend
    gvfsdbusutils.[ch] are renamed to gvfshalutils.[ch] to prevent confusion.
    
    https://bugzilla.gnome.org/show_bug.cgi?id=722411

--- common/gvfshalutils.c
+++ common/gvfshalutils.c
@@ -26,7 +26,7 @@
 #include <string.h>
 
 #include <glib/gi18n-lib.h>
-#include <gvfsdbusutils.h>
+#include <gvfshalutils.h>
 #include <gio/gio.h>
 
 static void
--- monitor/gphoto2/hal-pool.c
+++ monitor/gphoto2/hal-pool.c
@@ -23,7 +23,7 @@
 #include <string.h>
 #include <sys/time.h>
 
-#include <gvfsdbusutils.h>
+#include <gvfshalutils.h>
 
 #include "hal-pool.h"
 #include "hal-marshal.h"
