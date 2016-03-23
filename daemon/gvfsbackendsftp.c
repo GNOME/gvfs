88f91664f6200e7a2e564bc75c16957b2c7149a3
Ondrej Holy <oholy@redhat.com>
2016-03-23 09:09:16 +0100

    sftp: Fix error code if ssh is not properly configured
    G_IO_ERROR_PERMISSION_DENIED is returned if ssh is not properly configured.
    Nautilus shows "Don't have permission to access the requested location" in
    such case rather than "Connection refused by server", which is confusing.
    G_IO_ERROR_CONNECTION_REFUSED should be returned instead.
    
    https://bugzilla.gnome.org/show_bug.cgi?id=764049

--- daemon/gvfsbackendsftp.c
+++ daemon/gvfsbackendsftp.c
@@ -355,7 +355,7 @@ look_for_stderr_errors (Connection *conn, GError **error)
                strstr (line, "subsystem request failed") != NULL)
         {
           g_set_error_literal (error,
-	                       G_IO_ERROR, G_IO_ERROR_PERMISSION_DENIED,
+                               G_IO_ERROR, G_IO_ERROR_CONNECTION_REFUSED,
         	               _("Connection refused by server"));
           return;
         }
