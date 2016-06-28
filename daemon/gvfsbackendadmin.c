48ce5be3d3b073ef44a6989bd8718db3e998ed9f
Cosimo Cecchi <cosimoc@gnome.org>
2016-06-28 07:01:21 -0700

    admin: add more read/write flag overrides
    We can also delete/rename files when going through the admin backend.

--- daemon/gvfsbackendadmin.c
+++ daemon/gvfsbackendadmin.c
@@ -189,6 +189,10 @@ do_query_info (GVfsBackend *backend,
     (real_info, G_FILE_ATTRIBUTE_ACCESS_CAN_READ, TRUE);
   g_file_info_set_attribute_boolean
     (real_info, G_FILE_ATTRIBUTE_ACCESS_CAN_WRITE, TRUE);
+  g_file_info_set_attribute_boolean
+    (real_info, G_FILE_ATTRIBUTE_ACCESS_CAN_DELETE, TRUE);
+  g_file_info_set_attribute_boolean
+    (real_info, G_FILE_ATTRIBUTE_ACCESS_CAN_RENAME, TRUE);
 
   g_file_info_copy_into (real_info, info);
   g_object_unref (real_info);
