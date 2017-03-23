GNOME/gvfs

commit 60f96c8bd48e25ecdec5b9d8d3a3f86bfb119d3e

gphoto2: Improve root dir name and icon handling
Set the name of the root dir and the icon to match the way that the
volume monitor sets them.

This is visible as the folder title when the root dir of the device is
open in Nautilus (or using gvfs-info).

https://bugzilla.gnome.org/show_bug.cgi?id=736285

daemon/gvfsbackendgphoto2.c
@@ -62,6 +62,7 @@
 #include "gvfsmonitor.h"
 #include "gvfsjobseekwrite.h"
 #include "gvfsicon.h"
+#include "gvfsgphoto2utils.h"
 
 /* showing debug traces */
 #if 1
@@ -727,19 +728,8 @@ compute_display_name (GVfsBackendGphoto2 *gphoto2_backend)
   char *result = NULL;
 
 #ifdef HAVE_GUDEV
-  const char *s;
-
-  /* the real "nice" and user-visible name is computed in the monitor; just try
-   * using the product name here */
   if (gphoto2_backend->udev_device != NULL)
-    {
-      s = g_udev_device_get_sysfs_attr (gphoto2_backend->udev_device, "product");
-      if (s == NULL)
-        s = g_udev_device_get_property (gphoto2_backend->udev_device, "ID_MODEL");
-
-      if (s != NULL)
-        result = g_strdup (s);
-    }
+    result = g_vfs_get_volume_name (gphoto2_backend->udev_device, "ID_GPHOTO2");
   if (result == NULL )
     {
       /* Translator: %s represents the device, e.g. usb:001,042  */
@@ -791,24 +781,8 @@ setup_for_device (GVfsBackendGphoto2 *gphoto2_backend)
     {
       DEBUG ("-> sysfs path %s, subsys %s, name %s\n", g_udev_device_get_sysfs_path (gphoto2_backend->udev_device), g_udev_device_get_subsystem (gphoto2_backend->udev_device), g_udev_device_get_name (gphoto2_backend->udev_device));
 
-      /* determine icon name */
-      if (g_udev_device_has_property (gphoto2_backend->udev_device, "ID_MEDIA_PLAYER_ICON_NAME"))
-	{
-          gphoto2_backend->icon_name = g_strdup (g_udev_device_get_property (gphoto2_backend->udev_device, "ID_MEDIA_PLAYER_ICON_NAME"));
-          gphoto2_backend->symbolic_icon_name = g_strdup ("multimedia-player-symbolic");
-	  is_media_player = TRUE;
-	}
-      else if (g_udev_device_has_property (gphoto2_backend->udev_device, "ID_MEDIA_PLAYER"))
-	{
-          gphoto2_backend->icon_name = g_strdup ("multimedia-player");
-          gphoto2_backend->symbolic_icon_name = g_strdup ("multimedia-player-symbolic");
-	  is_media_player = TRUE;
-	}
-      else
-        {
-          gphoto2_backend->icon_name = g_strdup ("camera-photo");
-          gphoto2_backend->symbolic_icon_name = g_strdup ("camera-photo-symbolic");
-        }
+      gphoto2_backend->icon_name = g_vfs_get_volume_icon (gphoto2_backend->udev_device);
+      gphoto2_backend->symbolic_icon_name = g_vfs_get_volume_symbolic_icon (gphoto2_backend->udev_device);
     }
   else
       DEBUG ("-> did not find matching udev device\n");

daemon/Makefile.am
@@ -478,7 +478,8 @@ gvfsd_gphoto2_CPPFLAGS += $(HAL_CFLAGS)
 endif
 
 if USE_GUDEV
-gvfsd_gphoto2_LDADD = $(libraries) $(GPHOTO2_LIBS) $(GUDEV_LIBS)
+gvfsd_gphoto2_LDADD = $(libraries) $(GPHOTO2_LIBS) $(GUDEV_LIBS) \
+		      $(top_builddir)/common/libgvfscommon-gphoto2.la
 else
 gvfsd_gphoto2_LDADD = $(libraries) $(GPHOTO2_LIBS) $(HAL_LIBS)
 endif

commit f2f39758482a71eff4da399f47ae702ef1fa2857

gphoto2: Fix x-content-types setup
Commit 60f96c8 broke x-content-types setup, because is_media_player is
always FALSE. Let's introduce g_vfs_backend_set_x_content_types helper
and set x-content-types properly.

This issue was revealed by coverity scan.

common/gvfsgphoto2utils.c
@@ -140,3 +140,15 @@ g_vfs_get_volume_symbolic_icon (GUdevDevice *device)
   else
     return g_strdup ("camera-photo-symbolic");
 }
+
+char **
+g_vfs_get_x_content_types (GUdevDevice *device)
+{
+  char *camera_x_content_types[] = {"x-content/image-dcf", NULL};
+  char *media_player_x_content_types[] = {"x-content/audio-player", NULL};
+
+  if (g_udev_device_has_property (device, "ID_MEDIA_PLAYER"))
+    return g_strdupv (media_player_x_content_types);
+  else
+    return g_strdupv (camera_x_content_types);
+}

common/gvfsgphoto2utils.h
@@ -26,5 +26,6 @@
 char * g_vfs_get_volume_name (GUdevDevice *device, const char *device_id);
 char * g_vfs_get_volume_icon (GUdevDevice *device);
 char * g_vfs_get_volume_symbolic_icon (GUdevDevice *device);
+char **g_vfs_get_x_content_types (GUdevDevice *device);
 
 #endif

daemon/gvfsbackendgphoto2.c
@@ -696,9 +696,6 @@ setup_for_device (GVfsBackendGphoto2 *gphoto2_backend)
 {
   gchar *devname;
   char *comma;
-  gboolean is_media_player = FALSE;
-  char *camera_x_content_types[] = {"x-content/image-dcf", NULL};
-  char *media_player_x_content_types[] = {"x-content/audio-player", NULL};
 
   /* turn usb:001,041 string into an udev device name */
   if (!g_str_has_prefix (gphoto2_backend->gphoto2_port, "usb:"))
@@ -725,10 +722,8 @@ setup_for_device (GVfsBackendGphoto2 *gphoto2_backend)
   else
       g_debug ("-> did not find matching udev device\n");
 
-  if (is_media_player)
-      g_vfs_backend_set_x_content_types (G_VFS_BACKEND (gphoto2_backend), media_player_x_content_types);
-  else
-      g_vfs_backend_set_x_content_types (G_VFS_BACKEND (gphoto2_backend), camera_x_content_types);
+  g_vfs_backend_set_x_content_types (G_VFS_BACKEND (gphoto2_backend),
+                                     g_vfs_get_x_content_types (gphoto2_backend->udev_device));
 }
 
 static void