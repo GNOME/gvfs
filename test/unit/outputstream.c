a4dc329aae59a65815718ba2125df04ba3a9e8da
Ondrej Holy <oholy@redhat.com>
2024-11-28 11:12:08 +0100

    test: Add unit tests for edit mode
    Let's add tests for the newly added edit mode to be sure it is correctly
    implemented.

--- test/unit/outputstream.c
+++ test/unit/outputstream.c
@@ -27,6 +27,9 @@
 #define TEST_BUFFER2 "0123456789"
 #define TEST_BUFFER3 "helloworld"
 
+#define _g_file_edit(file, flags, cancellable, error) \
+  g_file_append_to(file, flags | (1 << 15), cancellable, error)
+
 static GFile *
 get_test_file (GFile *test_dir,
                const gchar *name,
@@ -603,6 +606,201 @@ test_append_tell (gconstpointer user_data)
 }
 
 static void
+test_edit_nonexistent (gconstpointer user_data)
+{
+  GFile *test_dir = G_FILE (user_data);
+  g_autoptr(GFile) test_file = NULL;
+  g_autoptr(GError) error = NULL;
+  g_autoptr(GFileOutputStream) output_stream = NULL;
+
+  g_test_summary ("It verifies that _g_file_edit() creates a file when it "
+                  "doesn't exist yet.");
+
+  /* Prepare */
+  test_file = get_test_file (test_dir,
+                             "test_edit_nonexistent",
+                             NULL,
+                             FALSE);
+
+  /* Test */
+  output_stream = _g_file_edit (test_file, G_FILE_CREATE_NONE, NULL, &error);
+  if (g_error_matches (error, G_IO_ERROR, G_IO_ERROR_NOT_SUPPORTED))
+    {
+      g_test_skip ("Edit is not supported.");
+      return;
+    }
+
+  g_assert_no_error (error);
+  g_assert_nonnull (output_stream);
+
+  test_write_helper (test_file, output_stream, TEST_BUFFER);
+}
+
+static void
+test_edit_existent_file (gconstpointer user_data)
+{
+  GFile *test_dir = G_FILE (user_data);
+  g_autoptr(GFile) test_file = NULL;
+  g_autoptr(GError) error = NULL;
+  g_autoptr(GFileOutputStream) output_stream = NULL;
+
+  g_test_summary ("It verifies that _g_file_edit() edits data when the "
+                  "file already exists.");
+
+  /* Prepare */
+  test_file = get_test_file (test_dir,
+                             "test_edit_existent_file",
+                             TEST_BUFFER2 TEST_BUFFER2 TEST_BUFFER2,
+                             FALSE);
+
+  /* Test */
+  output_stream = _g_file_edit (test_file, G_FILE_CREATE_NONE, NULL, &error);
+  if (g_error_matches (error, G_IO_ERROR, G_IO_ERROR_NOT_SUPPORTED))
+    {
+      g_test_skip ("Edit is not supported.");
+      return;
+    }
+
+  g_assert_no_error (error);
+  g_assert_nonnull (output_stream);
+
+  test_write_helper (test_file,
+                     output_stream,
+                     TEST_BUFFER "6789");
+}
+
+static void
+test_edit_existent_dir (gconstpointer user_data)
+{
+  GFile *test_dir = G_FILE (user_data);
+  g_autoptr(GFile) test_file = NULL;
+  g_autoptr(GError) error = NULL;
+  g_autoptr(GFileOutputStream) output_stream = NULL;
+
+  g_test_summary ("It verifies that _g_file_edit() fails with "
+                  "G_IO_ERROR_IS_DIRECTORY when there is a dir.");
+
+  /* Prepare */
+  test_file = get_test_file (test_dir, "test_edit_existent_dir", NULL, TRUE);
+
+  /* Test */
+  output_stream = _g_file_edit (test_file, G_FILE_CREATE_NONE, NULL, &error);
+  if (g_error_matches (error, G_IO_ERROR, G_IO_ERROR_NOT_SUPPORTED))
+    {
+      g_test_skip ("Edit is not supported.");
+      return;
+    }
+
+  g_assert_error (error, G_IO_ERROR, G_IO_ERROR_IS_DIRECTORY);
+  g_assert_null (output_stream);
+
+  /* Clean */
+  g_file_delete (test_file, NULL, NULL);
+}
+
+static void
+test_edit_seek (gconstpointer user_data)
+{
+  g_autoptr(GError) error = NULL;
+  GFile *test_dir = G_FILE (user_data);
+  g_autoptr(GFile) test_file = NULL;
+  g_autoptr(GFileOutputStream) output_stream = NULL;
+
+  g_test_summary ("It verifies that seek works on a stream created by "
+                  "_g_file_edit().");
+
+  /* Prepare */
+  test_file = get_test_file (test_dir, "test_edit_seek", NULL, FALSE);
+
+  /* Test */
+  output_stream = _g_file_edit (test_file, G_FILE_CREATE_NONE, NULL, &error);
+  if (g_error_matches (error, G_IO_ERROR, G_IO_ERROR_NOT_SUPPORTED))
+    {
+      g_test_skip ("Edit is not supported.");
+      return;
+    }
+
+  g_assert_no_error (error);
+  g_assert_nonnull (output_stream);
+
+  test_seek_helper (test_file, output_stream, "abcde1ghijk2mnopqrstu3wxyz");
+}
+
+static void
+test_edit_truncate (gconstpointer user_data)
+{
+  GFile *test_dir = G_FILE (user_data);
+  g_autoptr(GFile) test_file = NULL;
+  g_autoptr(GError) error = NULL;
+  g_autoptr(GFileOutputStream) output_stream = NULL;
+
+  g_test_summary ("It verifies that truncate works on a stream created by "
+                  "_g_file_edit().");
+
+  /* Prepare */
+  test_file = get_test_file (test_dir,
+                             "test_edit_truncate",
+                             TEST_BUFFER3,
+                             FALSE);
+
+  /* Test */
+  output_stream = _g_file_edit (test_file, G_FILE_CREATE_NONE, NULL, &error);
+  if (g_error_matches (error, G_IO_ERROR, G_IO_ERROR_NOT_SUPPORTED))
+    {
+      g_test_skip ("Edit is not supported.");
+      return;
+    }
+
+  g_assert_no_error (error);
+  g_assert_nonnull (output_stream);
+
+  test_truncate_helper (test_file,
+                        output_stream,
+                        "abcde\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0"
+                        TEST_BUFFER2,
+                        32);
+}
+
+static void
+test_edit_tell (gconstpointer user_data)
+{
+  g_autoptr(GError) error = NULL;
+  GFile *test_dir = G_FILE (user_data);
+  g_autoptr(GFile) test_file = NULL;
+  g_autoptr(GFileOutputStream) output_stream = NULL;
+
+  g_test_summary ("It verifies that offset is correctly reported on a stream "
+                   "created by _g_file_edit().");
+
+  /* Prepare */
+  test_file = get_test_file (test_dir, "test_edit_tell", TEST_BUFFER, FALSE);
+
+  /* Test */
+  output_stream = _g_file_edit (test_file,
+                                G_FILE_CREATE_NONE,
+                                NULL,
+                                &error);
+  if (g_error_matches (error, G_IO_ERROR, G_IO_ERROR_NOT_SUPPORTED))
+    {
+      g_test_skip ("Edit is not supported.");
+      return;
+    }
+
+  g_assert_no_error (error);
+  g_assert_nonnull (output_stream);
+
+  if (!g_seekable_can_seek (G_SEEKABLE (output_stream)))
+    {
+      g_test_skip ("Seek is not supported.");
+      return;
+    }
+
+  g_assert_cmpint (g_seekable_tell (G_SEEKABLE (output_stream)),
+                   ==,
+                   0);
+}
+
+static void
 test_replace_nonexistent (gconstpointer user_data)
 {
   GFile *test_dir = G_FILE (user_data);
@@ -844,6 +1042,28 @@ main (int argc, char *argv[])
   g_test_add_data_func ("/write/append-tell",
                         test_dir,
                         test_append_tell);
+
+  if (!g_file_has_uri_scheme (test_dir, "file"))
+    {
+      g_test_add_data_func ("/write/edit-nonexistent",
+                            test_dir,
+                            test_edit_nonexistent);
+      g_test_add_data_func ("/write/edit-existent-file",
+                            test_dir,
+                            test_edit_existent_file);
+      g_test_add_data_func ("/write/edit-existent-dir",
+                            test_dir,
+                            test_edit_existent_dir);
+      g_test_add_data_func ("/write/edit-seek",
+                            test_dir,
+                            test_edit_seek);
+      g_test_add_data_func ("/write/edit-truncate",
+                            test_dir,
+                            test_edit_truncate);
+      g_test_add_data_func ("/write/edit-tell",
+                            test_dir,
+                            test_edit_tell);
+    }
 
   g_test_add_data_func ("/write/replace-nonexistent",
                         test_dir,
