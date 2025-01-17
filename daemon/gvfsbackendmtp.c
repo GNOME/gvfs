a4c975c4137b99e6efff8bd78b35e099564d4f98
Ondrej Holy <oholy@redhat.com>
2025-01-17 15:14:23 +0100

    mtp: Use common error message for G_IO_ERROR_IS_DIRECTORY
    The commit 8c47265 added new `File is a directory` string by mistake.
    Let's use the `Target file is a directory` instead to avoid string break.
    
    Fixes: https://gitlab.gnome.org/Teams/Translation/Coordination/-/issues/230

--- daemon/gvfsbackendmtp.c
+++ daemon/gvfsbackendmtp.c
@@ -2747,7 +2747,7 @@ do_append_to (GVfsBackend *backend,
   if (file->filetype == LIBMTP_FILETYPE_FOLDER) {
     g_vfs_job_failed_literal (G_VFS_JOB (job),
                               G_IO_ERROR, G_IO_ERROR_IS_DIRECTORY,
-                              _("File is a directory"));
+                              _("Target file is a directory"));
     LIBMTP_destroy_file_t (file);
     goto exit;
   }
@@ -2824,7 +2824,7 @@ do_replace (GVfsBackend *backend,
   if (file->filetype == LIBMTP_FILETYPE_FOLDER) {
     g_vfs_job_failed_literal (G_VFS_JOB (job),
                               G_IO_ERROR, G_IO_ERROR_IS_DIRECTORY,
-                              _("File is a directory"));
+                              _("Target file is a directory"));
     LIBMTP_destroy_file_t (file);
     goto exit;
   }
